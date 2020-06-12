0b2deb31ebf41115c97a0a4d79e76f219a1c7d92
patklaey <pat@247.ch>
2020-06-12 17:34:04 +0200

    Updating corenerstone plugin

--- wordpress/cornerstone/js/lib.posts.inline_edit.js
+++ wordpress/cornerstone/js/lib.posts.inline_edit.js
@@ -1,5 +1,4 @@
 (function($) {
-
 if ( inlineEditPost && CNR && CNR.posts ) {
 	//Move init method to guarantee execution order
 	inlineEditPost.initSaved = inlineEditPost.init;
@@ -13,25 +12,33 @@ if ( inlineEditPost && CNR && CNR.posts ) {
 			this.field_parent = this.base.add_prefix('post_parent');
 			var t = this;
 			//Execute default init method
-			if ( inlineEditPost.initSaved )
+			if ( inlineEditPost.initSaved ) {
 				inlineEditPost.initSaved();
+			}
 			//Unbind quick edit click events
-			$('a.editinline').unbind('click');
+			$('.wp-list-table tbody').off( 'click',  '.editinline' );
 			//Bind new quick edit click handler
-			$('.wp-list-table tbody').on('click', 'a.editinline', function() { t.editHandler(this); return false; });
+			$('.wp-list-table tbody').on( 'click', '.editinline',
+				function() {
+					t.editHandler(this);
+					return false;
+				}
+			);
 			var qeRow = $('#inline-edit');
 			$('a.save', qeRow).click(function() { return t.save(this); });
 			$('td', qeRow).keydown(function(e) { if (e.which == 13) { return t.save(this); } });
 			//Restore original init method for future use
-			if ( inlineEditPost.initSaved )
+			if ( inlineEditPost.initSaved ) {
 				inlineEditPost.init = inlineEditPost.initSaved;
+			}
 		},
 	
 		save: function(id) {
 			var t = this, post_parent;
 			//Update post data
-			if (typeof(id) == 'object')
+			if (typeof(id) == 'object') {
 				id = t.getId(id);
+			}
 			if (id) {
 				//Get post parent
 				post_parent = $('#edit-' + id + ' #' + t.field_parent + ' option:selected');
@@ -51,8 +58,9 @@ if ( inlineEditPost && CNR && CNR.posts ) {
 		
 		preEdit: function(id) {
 			var t = this, post_id, section_select, parent_id;
-			if (typeof(id) == 'object')
+			if (typeof(id) == 'object') {
 				id = t.getId(id);
+			}
 			//Get master section selection
 			section_select = $('#inline-edit #' + t.field_parent);
 			//Get Parent ID
@@ -69,7 +77,8 @@ if ( inlineEditPost && CNR && CNR.posts ) {
 	});
 }
 
-if ( CNR && CNR.admin && CNR.posts.inline_edit && CNR.posts.inline_edit.init )
+if ( CNR && CNR.admin && CNR.posts.inline_edit && CNR.posts.inline_edit.init ) {
 	$(document).ready(function() { CNR.posts.inline_edit.init(); });
+}
 	
 })(jQuery);
